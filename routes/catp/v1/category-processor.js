fa188f58acedd225445f414d446fc1923b70dad0
vb <vibi94@gmail.com>
2019-11-17 15:43:00 +0700

    remove comment

--- routes/catp/v1/category-processor.js
+++ routes/catp/v1/category-processor.js
@@ -4,10 +4,4 @@ const { CategoryController } = require('../../../controller')
 category.get('/video/:categoryid', CategoryController.videocategory)
 category.get('/recommend/:userid', CategoryController.recommend)
 
-// category.post('/store', [CategoryMiddleware.checkValidation, CategoryMiddleware.checkDuplicate], CategoryController.create)
-// category.get('/search', CategoryController.search)
-// category.get('/:categoryid', CategoryController.show)
-// category.put('/update/:categoryid', [CategoryMiddleware.checkValidation, CategoryMiddleware.checkDuplicate], CategoryController.update)
-// category.delete('/delete/:categoryid', CategoryController.destroy)
-
 module.exports = category
